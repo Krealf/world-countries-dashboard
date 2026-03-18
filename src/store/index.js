7b062b5d4bad9a1af2bbdc1152989c5212f82370
krilph <krealf@mail.ru>
2026-03-18 11:56:11 +0300

    feat: add Boilerplate code for Redux

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+import {applyMiddleware, compose, createStore} from "redux";
+import thunk from "redux-thunk";
+
+import {rootReducer} from "./root-reducer";
+import * as api from "../config"
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(thunk.withExtraArgument({
+  client: axios,
+  api
+}))));
+
+export {store}
